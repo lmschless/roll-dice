7536084030803449211fa0b1912c8f5881af8895
Luke <lmschless@gmail.com>
2020-04-28 23:42:02 -0700

    have dice rolling when button is clicked. State is stored in rolldice.js, rand function is called on click.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RollDive from './RollDice';
+import RollDice from './RollDice';
 
 function App() {
 	return (
 		<React.Fragment>
-			<RollDive />
+			<RollDice />
 		</React.Fragment>
 	);
 }
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import DieOne from './DieOne';
+import './die.css';
 
 export default class RollDice extends Component {
-	static propTypes = {
-		prop: PropTypes,
+	constructor(props) {
+		super(props);
+		this.state = {
+			sideOne: 'fas fa-dice-one',
+			sideTwo: 'fas fa-dice-six'
+		};
+	}
+	roll = () => {
+		let sides = [
+			'fas fa-dice-one',
+			'fas fa-dice-two',
+			'fas fa-dice-three',
+			'fas fa-dice-four',
+			'fas fa-dice-five'
+		];
+		let randOne = sides[Math.floor(Math.random() * sides.length)];
+		let randTwo = sides[Math.floor(Math.random() * sides.length)];
+
+		this.setState({
+			sideOne: randOne,
+			sideTwo: randTwo
+		});
 	};
 
 	render() {
 		return (
-			<div>
-				<i class="fas fa-dice-one" />
-			</div>
+			<React.Fragment>
+				<div className=".Dice">
+					<DieOne face={this.state.sideOne} />
+					<DieOne face={this.state.sideTwo} />
+					{/* <i className="fas fa-dice-one" /> */}
+				</div>
+				<button onClick={this.roll}>Roll Dice</button>
+			</React.Fragment>
 		);
 	}
 }
