7232ac4eea00baa8a468d57179799b126a1b3c04
Luke <lmschless@gmail.com>
2020-04-28 22:48:26 -0700

    starting work on rolldice component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RollDive from './RollDice';
 
 function App() {
-	return <React.Fragment />;
+	return (
+		<React.Fragment>
+			<RollDive />
+		</React.Fragment>
+	);
 }
 
 export default App;
